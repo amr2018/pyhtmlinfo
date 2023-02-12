c85c645df580f95ee5b6250affb877543228bcd1
Riadh <riadh.azzoun@hotmail.com>
2023-02-12 01:39:56 +0100

    Refactored the code

--- pyhtmlinfo/__init__.py
+++ pyhtmlinfo/__init__.py
@@ -1,9 +1,1 @@
-
-from pyhtmlinfo import name_to_tag
-from pyhtmlinfo import tag_to_name
-from pyhtmlinfo import Deprecated_tags
-from pyhtmlinfo import Tags
-from pyhtmlinfo import get_tag_description
-from pyhtmlinfo import is_maybe_html_tag
-from pyhtmlinfo import get_all_tags
-from pyhtmlinfo import is_old_or_deprecated
+from pyhtmlinfo.src import HtmlInfo
